367749d2d431bfd3adc4a50d1a43071eecc1fea1
Ali127Dev <Ali127Dev@gmail.com>
2026-09-01 12:43:38 +0330

    feat: add version to base entity

--- src/main/java/io/github/ali127dev/springshop/shared/entity/BaseEntity.java
+++ src/main/java/io/github/ali127dev/springshop/shared/entity/BaseEntity.java
@@ -1,9 +1,6 @@
 package io.github.ali127dev.springshop.shared.entity;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.Id;
-import jakarta.persistence.MappedSuperclass;
+import jakarta.persistence.*;
 import java.time.Instant;
 import java.util.UUID;
 import lombok.Getter;
@@ -26,4 +23,6 @@ public class BaseEntity {
   private Instant createdAt;
 
   @UpdateTimestamp private Instant updatedAt;
+
+  @Version private Long version;
 }
