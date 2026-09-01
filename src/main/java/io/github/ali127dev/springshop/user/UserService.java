6ee1c60a7712bc139ad4fda556f8fc7af61cc77d
Ali127Dev <Ali127Dev@gmail.com>
2026-09-01 13:48:49 +0330

    feat: add password encoder

--- src/main/java/io/github/ali127dev/springshop/user/UserService.java
+++ src/main/java/io/github/ali127dev/springshop/user/UserService.java
@@ -2,17 +2,20 @@ package io.github.ali127dev.springshop.user;
 
 import io.github.ali127dev.springshop.user.dto.RegisterUser;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @RequiredArgsConstructor
 @Service
 public class UserService {
   private final UserRepository repository;
+  private final PasswordEncoder passwordEncoder;
 
   public User register(RegisterUser dto) {
     User user = new User();
     user.setName(dto.name());
     user.setEmail(dto.email());
+    user.setPassword(passwordEncoder.encode(dto.password()));
     user.setRole(UserRole.USER);
 
     return repository.save(user);
